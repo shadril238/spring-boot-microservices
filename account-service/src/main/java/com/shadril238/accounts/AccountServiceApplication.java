8118ec9090cd0fa9cb75ef7e3f1495e87d802e10
shadril238 <shadrilhassan@outlook.com>
2024-10-08 19:09:57 +0600

    Implemented documentation of APIs using springdoc OpenAPIDefinition

--- account-service/src/main/java/com/shadril238/accounts/AccountServiceApplication.java
+++ account-service/src/main/java/com/shadril238/accounts/AccountServiceApplication.java
@@ -1,15 +1,43 @@
 package com.shadril238.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.shadril238.accounts.controller") })
+@EnableJpaRepositories("com.shadril238.accounts.repository")
+@EntityScan("com.shadril238.accounts.entity")*/
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl") // Enable JPA Auditing
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Account microservice REST API documentation",
+                description = "Eazy Bank Account microservice REST API documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Shadril Hassan",
+                        email = "shadrilhassan@outlook.com",
+                        url = "brainstation-23.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "http://www.apache.org/licenses/LICENSE-2.0"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "EazyBank Accounts microservice REST API documentation",
+                url = "https://eazybank.com/swagger-ui.html"
+        )
+)
 public class AccountServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccountServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AccountServiceApplication.class, args);
+    }
 
 }
--- account-service/src/main/java/com/shadril238/accounts/dto/CustomerDto.java
+++ account-service/src/main/java/com/shadril238/accounts/dto/CustomerDto.java
@@ -1,5 +1,6 @@
 package com.shadril238.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
@@ -7,18 +8,34 @@ import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
+@Schema(
+        name = "Customer",
+        description = "Schema to hold Customer and Account information"
+)
 public class CustomerDto {
+    @Schema(
+            description = "Name of the customer", example = "Shadril Hassan"
+    )
     @NotEmpty(message = "Name can not be null or empty")
     @Size(min = 5, max = 30, message = "The length of the customer name should be between 5 and 30")
     private String name;
 
+    @Schema(
+            description = "Email address of the customer", example = "shadrilhassan@outlook.com"
+    )
     @NotEmpty(message = "Email can not be null or empty")
     @Email(message = "Email address should be valid value")
     private String email;
 
+    @Schema(
+            description = "Mobile Number of the customer", example = "9345432123"
+    )
     @NotEmpty(message = "Mobile Number can not be null or empty")
     @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile Number must be 10 digits")
     private String mobileNumber;
 
+    @Schema(
+            description = "Account details of the Customer"
+    )
     private AccountsDto accountsDto;
 }
