60d52113b3f771ab365a49fd89a93f416f6265c8
shadril238 <shadrilhassan@outlook.com>
2024-10-08 12:05:49 +0600

    Build account microservices

---
+++ account-service/src/main/java/com/shadril238/accounts/controller/CustomerDto.java
@@ -0,0 +1,4 @@
+package com.shadril238.accounts.controller;
+
+public class CustomerDto {
+}
---
+++ account-service/src/main/java/com/shadril238/accounts/repository/AccountsRepository.java
@@ -0,0 +1,9 @@
+package com.shadril238.accounts.repository;
+
+import com.shadril238.accounts.entity.Accounts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountsRepository extends JpaRepository<Accounts, Long> {
+}
