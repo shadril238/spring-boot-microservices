67f7f674df0e27ef9a9d575d135010200a67414c
shadril238 <shadrilhassan@outlook.com>
2024-10-09 14:47:15 +0600

    Implemented card-service microservice

--- card-service/src/main/java/com/shadril238/cards/CardServiceApplication.java
+++ card-service/src/main/java/com/shadril238/cards/CardServiceApplication.java
@@ -1,13 +1,43 @@
 package com.shadril238.cards;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.shadril238.cards.controller") })
+@EnableJpaRepositories("com.shadril238.cards.repository")
+@EntityScan("com.shadril238.cards.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Cards microservice REST API Documentation",
+                description = "EazyBank Cards microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Shadril Hassan",
+                        email = "shadrilhassan@outlook.com",
+                        url = "https://www.bs23.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.bs23.com"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "EazyBank Cards microservice REST API Documentation",
+                url = "https://www.bs23.com/swagger-ui.html"
+        )
+)
 public class CardServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CardServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CardServiceApplication.class, args);
+    }
 
 }
