0f6a7df77cd91230c917ce6cb17f8ec7316bdfe4
shadril238 <shadrilhassan@outlook.com>
2024-10-11 16:41:27 +0600

    Generate Docker Image of account-service with Dockerfile

--- account-service/src/test/java/com/shadril238/account_service/AccountServiceApplicationTests.java
+++ account-service/src/test/java/com/shadril238/account_service/AccountServiceApplicationTests.java
@@ -1,9 +1,12 @@
 package com.shadril238.account_service;
 
+import com.shadril238.accounts.AccountServiceApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = AccountServiceApplication.class)
 class AccountServiceApplicationTests {
 
 	@Test
