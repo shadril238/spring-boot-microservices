ef719e278129eff6c075d821c5df647338fbf9a3
shadril238 <shadrilhassan@outlook.com>
2024-10-11 17:31:38 +0600

    Generate Docker Image of card-service with Google Jib

--- card-service/src/test/java/com/shadril238/card_service/CardServiceApplicationTests.java
+++ card-service/src/test/java/com/shadril238/card_service/CardServiceApplicationTests.java
@@ -1,9 +1,12 @@
 package com.shadril238.card_service;
 
+import com.shadril238.cards.CardServiceApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = CardServiceApplication.class)
 class CardServiceApplicationTests {
 
 	@Test
