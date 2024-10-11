15bfd711a3e15b3d1c4ee32ce49df1f056345e93
shadril238 <shadrilhassan@outlook.com>
2024-10-11 17:13:09 +0600

    Generate Docker Image of loan-service with Buildpacks

--- loan-service/src/test/java/com/shadril238/loan_service/LoanServiceApplicationTests.java
+++ loan-service/src/test/java/com/shadril238/loan_service/LoanServiceApplicationTests.java
@@ -1,9 +1,12 @@
 package com.shadril238.loan_service;
 
+import com.shadril238.loans.LoanServiceApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = LoanServiceApplication.class)
 class LoanServiceApplicationTests {
 
 	@Test
