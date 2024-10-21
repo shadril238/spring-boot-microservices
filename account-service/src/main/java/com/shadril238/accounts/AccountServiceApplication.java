ad29334174a7a910b2e8029e62956587c2fb2cf9
shadril238 <shadrilhassan@outlook.com>
2024-10-21 17:37:52 +0600

    Reading configurations using ConfigurationProperties and Spring Boot Profiles

--- account-service/src/main/java/com/shadril238/accounts/AccountServiceApplication.java
+++ account-service/src/main/java/com/shadril238/accounts/AccountServiceApplication.java
@@ -1,5 +1,6 @@
 package com.shadril238.accounts;
 
+import com.shadril238.accounts.dto.AccountsContactInfoDto;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
@@ -7,6 +8,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -14,6 +16,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaRepositories("com.shadril238.accounts.repository")
 @EntityScan("com.shadril238.accounts.entity")*/
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl") // Enable JPA Auditing
+@EnableConfigurationProperties(value = {AccountsContactInfoDto.class})  // Enable Configuration Properties for AccountsContactInfoDto class to read from application.yml
 @OpenAPIDefinition(
         info = @Info(
                 title = "Account microservice REST API documentation",
--- account-service/src/main/java/com/shadril238/accounts/controller/AccountsController.java
+++ account-service/src/main/java/com/shadril238/accounts/controller/AccountsController.java
@@ -1,6 +1,7 @@
 package com.shadril238.accounts.controller;
 
 import com.shadril238.accounts.constants.AccountsConstants;
+import com.shadril238.accounts.dto.AccountsContactInfoDto;
 import com.shadril238.accounts.dto.CustomerDto;
 import com.shadril238.accounts.dto.ErrorResponseDto;
 import com.shadril238.accounts.dto.ResponseDto;
@@ -43,6 +44,9 @@ public class AccountsController {
 
     @Autowired
     private Environment environment;
+
+    @Autowired
+    private AccountsContactInfoDto accountsContactInfoDto;
 
     @GetMapping("/sayHello")
     public String sayHello() {
@@ -226,6 +230,31 @@ public class AccountsController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(environment.getProperty("JAVA_HOME"));
+    }
+
+    @Operation(
+            summary = "Get Contact Info",
+            description = "Contact Info details that can be reached out in case of any issues"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @GetMapping("/contact-info")
+    public ResponseEntity<AccountsContactInfoDto> getContactInfo() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(accountsContactInfoDto);
     }
 
 }
