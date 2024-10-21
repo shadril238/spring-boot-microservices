562d8c790274f723fb6f640a4fb1fc711016987b
shadril238 <shadrilhassan@outlook.com>
2024-10-21 18:11:22 +0600

    Spring Boot Profile changes inside Loans microservice

--- loan-service/src/main/java/com/shadril238/loans/LoanServiceApplication.java
+++ loan-service/src/main/java/com/shadril238/loans/LoanServiceApplication.java
@@ -1,5 +1,6 @@
 package com.shadril238.loans;
 
+import com.shadril238.loans.dto.LoansContactInfoDto;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
@@ -7,6 +8,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -14,6 +16,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaRepositories("com.shadril238.loans.repository")
 @EntityScan("com.shadril238.loans.model")*/
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = LoansContactInfoDto.class)
 @OpenAPIDefinition(
         info = @Info(
                 title = "Loans microservice REST API Documentation",
